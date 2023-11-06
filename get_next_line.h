e2d821c26686b5e6c4fa5d21737960159b1f2239
necati-vm-ubuntu <han.necati@gmail.com>
2023-11-06 21:35:56 +0100

    created get_next_line files

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhan <necat.han42@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 16:31:52 by nhan              #+#    #+#             */
+/*   Updated: 2023/11/06 16:52:28 by nhan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <limits.h>
+
+#endif
